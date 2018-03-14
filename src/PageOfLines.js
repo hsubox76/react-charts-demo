f1b634544edcfa958d26dd1de2b5d65c85b8cb23
Christina Holland <hsubox@gmail.com>
2018-03-14 14:47:13 -0700

    fix interactive button on lines page

--- src/PageOfLines.js
+++ src/PageOfLines.js
@@ -18,16 +18,16 @@ class PageOfLines extends Component {
     }
   }
   incrementEarth = () => {
-    const newEarth = Object.assign({}, this.state.data[0], { value: this.state.data[0].value + 10 });
+    const newPoint = [this.state.lineData[0], this.state.lineData[1] + 5];
     this.setState({
-      data: [newEarth].concat(this.state.data.slice(1))
+      lineData: this.state.lineData.map(point => [point[0], point[1] + Math.random() * 5 - 2.5])
     });
   };
   render() {
     return (
       <div className="charts-container">
         <button onClick={this.incrementEarth}>
-            more earth
+            new random data
         </button>
         <LineChart type="svg" data={this.state.lineData} height={140} />
         <LineChart type="canvas" data={this.state.lineData} height={140} />
